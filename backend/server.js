2f777ad84e2cffde05f448662fdf5daff138fca8
Iago <iagojlourenco@gmail.com>
2021-11-22 11:38:42 -0300

    clean

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import path from "path";
-import errorHandler from "./errors/handler";
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use("uploads", express.static(path.join(__dirname + "..", "uploads")));
