13bd508a2bbd4df9830eadf0531e5cf01dc2e9dd
derekfq <quaresmaderek@gmail.com>
2021-12-04 17:14:12 -0300

    Products example ,Header links and styles added

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
-import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import NextLink from 'next/link';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+} from '@material-ui/core';
 import useStyles from '../utils/styles';
 
 export default function Layout({ children }) {
@@ -13,7 +20,21 @@ export default function Layout({ children }) {
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <Typography>Tópicos Store</Typography>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={classes.brand}>Tópicos Store</Typography>
+            </Link>
+          </NextLink>
+          {/*esse div vai ocupar todo o width para jogar os outros divs no final*/}
+          <div className={classes.grow}></div>
+          <div>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/lart" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
